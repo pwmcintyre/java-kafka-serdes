38e136efcb361fc2114c47e3300711ecde314e34
peter <peter.mcintyre@gmail.com>
2024-09-29 23:07:21 +1000

    refactor: assert not null

--- serdes/src/main/java/com/pwmcintyre/kafka/serde/base64/Base64SerDe.java
+++ serdes/src/main/java/com/pwmcintyre/kafka/serde/base64/Base64SerDe.java
@@ -1,11 +1,15 @@
 package com.pwmcintyre.kafka.serde.base64;
 
 import java.util.Map;
+import java.util.Objects;
 
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serializer;
 
+/*
+ * A Serde that wraps a Serializer and Deserializer with Base64 encoding and decoding.
+ */
 public class Base64SerDe<T> implements Serde<T> {
 
     private final Base64Serializer<T> serializer;
@@ -16,13 +20,16 @@ public class Base64SerDe<T> implements Serde<T> {
     }
 
     public Base64SerDe(Serde<T> s) {
+        Objects.requireNonNull(s, "serde can't be null");
         serializer = new Base64Serializer<>(s.serializer());
         deserializer = new Base64Deserializer<>(s.deserializer());
     }
 
     public Base64SerDe(Serializer<T> s, Deserializer<T> d) {
-        serializer = new Base64Serializer(s);
-        deserializer = new Base64Deserializer(d);
+        Objects.requireNonNull(s, "serializer can't be null");
+        Objects.requireNonNull(d, "deserializer can't be null");
+        serializer = new Base64Serializer<>(s);
+        deserializer = new Base64Deserializer<>(d);
     }
 
     @Override
